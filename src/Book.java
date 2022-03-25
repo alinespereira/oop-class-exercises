6e5a9ab93ebc0de9f6681c8380c9e2a9b34b399a
Aline <line.asramos@gmail.com>
2022-03-24 21:04:48 -0300

    Setup and first exercise

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    private int nPages;
+    private String author;
+    private String title;
+    private int edition;
+
+    public int getNPages() {
+        return nPages;
+    }
+
+    public void setNPages(int nPages) {
+        this.nPages = nPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+    public void setEdition(int edition) {
+        this.edition = edition;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
